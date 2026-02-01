38d59eb7612c497d4c8f4a217dd840485736ab35
Sovik211 <vital.gurov2017@gmail.com>
2026-02-01 15:09:32 +0800

    Создание TaskEntity

---
+++ app/src/main/java/ru/example/to_dolist/data/database/entity/TaskEntity.kt
@@ -0,0 +1,21 @@
+package ru.example.to_dolist.data.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class TaskEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "description")
+    val description: String? = null,
+    @ColumnInfo(name = "is_completed")
+    val isCompleted: Boolean = false,
+    @ColumnInfo(name = "created_at")
+    val createdAt: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = "updated_at")
+    val updatedAt: Long = System.currentTimeMillis(),
+)
