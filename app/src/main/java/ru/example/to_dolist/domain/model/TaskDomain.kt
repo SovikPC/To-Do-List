1c83999f0245df1f2313940b51943317b05a7b08
Sovik211 <vital.gurov2017@gmail.com>
2026-03-09 11:13:13 +0800

    Добавление значения по умолчанию, изменение типа данных для createAt и updateAt Long -> LocalDateTime

--- app/src/main/java/ru/example/to_dolist/domain/model/TaskDomain.kt
+++ app/src/main/java/ru/example/to_dolist/domain/model/TaskDomain.kt
@@ -1,14 +1,15 @@
 package ru.example.to_dolist.domain.model
 
 import ru.example.to_dolist.data.database.entity.TaskEntity
+import java.time.LocalDateTime
 
 data class TaskDomain(
     val id: Long = 0,
-    val title: String,
-    val description: String? = null,
+    val title: String = "",
+    val description: String = "",
     val isCompleted: Boolean = false,
-    val createdAt: Long = System.currentTimeMillis(),
-    val updatedAt: Long = System.currentTimeMillis(),
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    val updatedAt: LocalDateTime = LocalDateTime.now(),
 ){
     fun toEntity(): TaskEntity = TaskEntity(
         id = id,
