1e7110af1f775855d91290886305367834e5540a
Sovik211 <vital.gurov2017@gmail.com>
2026-03-09 11:02:48 +0800

    Обновление версии базы данных, добавление DateTimeConverter

--- app/src/main/java/ru/example/to_dolist/data/database/AppDatabase.kt
+++ app/src/main/java/ru/example/to_dolist/data/database/AppDatabase.kt
@@ -2,16 +2,19 @@ package ru.example.to_dolist.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import ru.example.to_dolist.data.database.conventer.DateTimeConverter
 import ru.example.to_dolist.data.database.dao.TaskDao
 import ru.example.to_dolist.data.database.entity.TaskEntity
 
 @Database(
-    version = 1,
+    version = 3,
     exportSchema = true,
     entities = [
         TaskEntity::class
     ]
 )
+@TypeConverters(DateTimeConverter::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun taskDao(): TaskDao
 }
